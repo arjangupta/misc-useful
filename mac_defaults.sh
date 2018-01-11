0bdc763105474fc17769b8002efff38d653552dc
Gupta, Arjan <arjan.gupta@eyeverify.com>
2018-01-11 02:00:11 -0600

    Commentify

--- mac_defaults.sh
+++ mac_defaults.sh
@@ -1,6 +1,8 @@
-# These commands will enable fast key repeats, log out and log in for them to take effect
+# Log out and log back in for these commands to take effect as defaults 
+
+# These commands will enable fast key repeats
 defaults write NSGlobalDomain InitialKeyRepeat 15
 defaults write NSGlobalDomain KeyRepeat 2
 
-# This command will show all hidden files in Finder
+# This command will show all hidden files in Finder, in case you're sick of cmd + .
 defaults write com.apple.finder AppleShowAllFiles YES
