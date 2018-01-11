a6dec0aed8a7d1dc2cde155e88f584113330c038
Gupta, Arjan <arjan.gupta@eyeverify.com>
2018-01-11 01:54:33 -0600

    Edited a comment

--- mac_defaults.sh
+++ mac_defaults.sh
@@ -1,5 +1,4 @@
-# These commands will enable fast key repeats, log out and log in
-# for them to take effect
+# These commands will enable fast key repeats, log out and log in for them to take effect
 defaults write NSGlobalDomain InitialKeyRepeat 15
 defaults write NSGlobalDomain KeyRepeat 2
 
