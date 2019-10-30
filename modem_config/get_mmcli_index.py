fdfdff93d4d2a90c78ac64f83b229c0d241cbd65
Arjan Gupta <agupta@intellifarms.com>
2019-10-30 11:11:12 -0500

    Improve the check for sysargv and remove a superfluous check on the input str

--- modem_config/get_mmcli_index.py
+++ modem_config/get_mmcli_index.py
@@ -5,27 +5,22 @@ def main():
 
     search_str = '/Modem/'
     
-    if sys.argv[1] == '-b':
+    if len(sys.argv) > 1 and sys.argv[1] == '-b':
         search_str = '/Bearer/'
 
     search_result = input_str.find(search_str)
 
     if search_result == -1:
-        # Substring "/Modem/" not found
+        # Substring '/Modem/' not found
         print(101)
         return
-    elif search_result + len(search_str) == len(input_str):
-        # /Modem/ is the end of the input string'
-        print(102)
-        return
 
-    # TODO: Make sure to get 2 digit modem indices too - try getting substring
-    mmcli_index = input_str[search_result + len(search_str)]
+    # Get whatever is after the '/Modem/' and try to convert it to int
+    mmcli_index = int(input_str[(search_result + len(search_str)):])
     
     # Print modem index value
-    print(mmcli)
+    print(mmcli_index)
     return
-
 
 if __name__ == '__main__':
     main()
